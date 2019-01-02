c788852262af0a67ab13c4082420d8f1d4aedfe7
Mehmood Qadri <mehmood.qadri21@gmail.com>
2019-01-01 18:19:32 -0600

    Collatz Conjecture

--- main.cpp
+++ main.cpp
@@ -2,21 +2,25 @@
 using namespace std;
 
 int main() {
-	double interestRate, monthlyRate, loan;
-	int months;
-	cout << "Please input the interest rate for your loan: "; cin >> interestRate;
-	monthlyRate = interestRate / 12 / 100;
-	
-	cout << "Please input the loan amount: "; cin >> loan;
-	cout << "Please input the term of the loan in terms of years: "; cin >> months;
-	months = months * 12;
-	double monthlyPayment;
+	cout << "Please input a number greater than 1: ";
+	int n; cin >> n;
+	int steps = 0;
 
-	monthlyPayment = pow(1 + monthlyRate, -months);
-	monthlyPayment = 1 - monthlyPayment;
-	monthlyPayment = monthlyRate / monthlyPayment;
-	monthlyPayment = monthlyPayment * loan;
+	while (n <= 1) {
+		cout << "ERROR: Please input a number greater than 1" << endl;
+		cin >> n;
+	}
 
-	cout << "You will end up paying $" << monthlyPayment << " per month and will pay back the loan in " << (months / 12) << " years" << endl;
+	while (n != 1) {
+		if (n % 2 == 0) {
+			n = n / 2;
+		}
+		else {
+			n = (3 * n) + 1;
+		}
+		steps++;
+	}
+
+	cout << steps << endl;
 	system("pause");
 }
